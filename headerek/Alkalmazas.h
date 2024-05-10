bf5d84c930f5a7b2ecf7107fedca7b760e45a789
medveparancsnok <benedek.kiszely@outlook.hu>
2024-05-10 11:49:35 +0200

    Frissítés

---
+++ headerek/Alkalmazas.h
@@ -0,0 +1,28 @@
+
+
+#ifndef NHF_UJRA_ALKALMAZAS_H
+#define NHF_UJRA_ALKALMAZAS_H
+#include "FoMenu.h"
+
+
+/// @brief Osztály, ami a játék grafikus megjelenítésért felelős és a játék, illetve menü közötti váltást teszi lehetővé
+
+class Alkalmazas {
+    sf::RenderWindow ablak;
+    std::stack<Menu *> stack;
+    sf::Font font;
+public:
+
+    /// @brief Konstruktor, ami egy új alkalmazás objektumot hoz létre, betölti a feliratokhoz használt fontot,
+    /// továbbá az ablakot beállítja, amin a játék megjelenítése történik, illetve létrehoz egy FoMenu objektumot
+    /// a stacken
+    Alkalmazas();
+
+    /// @brief Futtatja az applikációt, eseményeket fogad, amik az ablak felé irányulnak, ha valaki a bezárást
+    /// választja, akkor a stacket kiüríti, majd bezárja az applikációt, egyébként a stack tetején lévő objektum eseménykezelő
+    /// függvényét hívja meg, majd ez után frissíti az ablakot és meghívja a stack legtetején lévő objektum megjelenítő függvényét
+    void Run();
+};
+
+
+#endif //NHF_UJRA_ALKALMAZAS_H
---
+++ src/Alkalmazas.cpp
@@ -0,0 +1,49 @@
+
+#include "../headerek/Alkalmazas.h"
+#include <iostream>
+
+Alkalmazas::Alkalmazas():ablak(sf::VideoMode(800,600), "Aknakereso", sf::Style::Default) {
+    ablak.setFramerateLimit(60);
+    if(!font.loadFromFile("resource/arial.ttf")){
+        throw "Nem sikerult Font-ot betolteni";
+    }
+    FoMenu *fomenu = new FoMenu(stack, font);
+    stack.push(fomenu);
+}
+
+void Alkalmazas::Run(){
+
+
+    while(ablak.isOpen()){
+        sf::Event esemeny;
+        while(ablak.pollEvent(esemeny)){
+            switch (esemeny.type) {
+                case sf::Event::Closed:{
+                    while(!stack.empty()){
+                        stack.pop();
+                    }
+                    ablak.close();
+                }
+                break;
+                case sf::Event::MouseButtonPressed:{
+                    stack.top()->esemeny_kezel(esemeny);
+                }
+                break;
+                default:{}
+                break;
+            }
+        }
+
+        ablak.clear(sf::Color(192, 192, 192));
+
+        if(!stack.empty()) {
+            stack.top()->megjelenit(ablak);
+        }
+
+        ablak.display();
+
+
+    }
+
+
+}
